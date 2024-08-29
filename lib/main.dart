01dd359f5f7cf3992dcb642d8bdaf6383387a2ed
sargeliu <53758352+Sargeliu@users.noreply.github.com>
2024-08-29 16:37:59 +0500

    применил кастомную текстовую тему (TextTheme) с использованием пакета google_fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:visa_in_usa/widgets/footer/footer.dart';
-
+import 'package:visa_in_usa/widgets/main_header.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,16 +17,32 @@ class MyApp extends StatelessWidget {
       title: 'Visa in Usa',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          textTheme: GoogleFonts.montserratTextTheme(
-              Theme.of(context).textTheme
-          ).apply(
-              bodyColor: Colors.black
-          )
-      ),
+          textTheme:
+              GoogleFonts.montserratTextTheme(Theme.of(context).textTheme)
+                  .copyWith(
+                bodyLarge: GoogleFonts.montserrat(fontSize: 16, fontWeight:
+                FontWeight.normal),
+                bodyMedium: GoogleFonts.montserrat(fontSize: 14, fontWeight:
+                FontWeight.normal),
+                displayLarge: GoogleFonts.montserrat(fontSize: 96, fontWeight:
+                FontWeight.bold),
+                displayMedium: GoogleFonts.montserrat(fontSize: 60, fontWeight: FontWeight.bold),
+                displaySmall: GoogleFonts.montserrat(fontSize: 48, fontWeight: FontWeight.bold),
+                headlineMedium: GoogleFonts.montserrat(fontSize: 34, fontWeight: FontWeight.bold),
+                headlineSmall: GoogleFonts.montserrat(fontSize: 24, fontWeight: FontWeight.bold),
+                titleLarge: GoogleFonts.montserrat(fontSize: 20, fontWeight: FontWeight.bold),
+                titleMedium: GoogleFonts.montserrat(fontSize: 16, fontWeight: FontWeight.w500),
+                titleSmall: GoogleFonts.montserrat(fontSize: 14, fontWeight: FontWeight.w500),
+                labelLarge: GoogleFonts.montserrat(fontSize: 14, fontWeight: FontWeight.bold),
+                bodySmall: GoogleFonts.montserrat(fontSize: 12, fontWeight: FontWeight.normal),
+                labelSmall: GoogleFonts.montserrat(fontSize: 10, fontWeight: FontWeight.normal),
+              )
+                  .apply(bodyColor: Colors.black)),
       home: Scaffold(
         backgroundColor: Colors.white,
         body: ListView(
           children: const [
+            MainHeader(),
             Footer(),
           ],
         ),
@@ -34,4 +50,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
